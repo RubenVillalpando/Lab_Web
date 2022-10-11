42d9523b81cdb9f3f9459f763a9a520c08bbe13a
Ruben Villalpando <mr.villalpando@yahoo.com.mx>
2022-10-10 21:07:49 -0500

    functional delivery for tomorrows class

--- practica-consulta-videojuegos/src/componentes/ResultadoVideojuegos.js
+++ practica-consulta-videojuegos/src/componentes/ResultadoVideojuegos.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useFetch } from "../customHooks/useFetch";
+import { useFetch } from "../customHooks/usefetch";
 import { InfoVideojuego } from "./InfoVideojuegos";
 
 export const ResultadoVideojuegos = (props) => {
